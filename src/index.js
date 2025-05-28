a852073bd96f2e929ee4ffd318337d4b5a94ec79
Alejandro Gomez  T. <alejogomezt2000@gmail.com>
2025-05-27 22:29:57 -0300

    Se restablece socket.io Mejora la comunicación en tiempo real.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const notificationService = require("./services/notificationService");
 
 const server = http.createServer(app);
 
-/*const io = new Server(server, {
+const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -22,10 +22,9 @@ const server = http.createServer(app);
 });
 
 socket.init(io);
-app.set("io", socket.getIo());*/
+app.set("io", socket.getIo());
 
 const setupSubscriber = async () => {
-  // Parsear DATABASE_URL
   const parsedConfig = parse(process.env.DATABASE_URL);
 
   const subscriber = createSubscriber({
@@ -81,7 +80,7 @@ const setupSubscriber = async () => {
 
 setupSubscriber();
 
-/*io.use((socket, next) => {
+io.use((socket, next) => {
   const token = socket.handshake.query.token;
   if (!token) {
     return next(new Error("Token requerido"));
@@ -94,11 +93,11 @@ setupSubscriber();
   } catch (error) {
     next(new Error("Token inválido"));
   }
-});*/
+});
 
 const clients = new Map();
 
-/*io.on("connection", (socket) => {
+io.on("connection", (socket) => {
   console.log(`Usuario ${socket.userId} conectado (Rol: ${socket.role})`);
 
   socket.join(socket.userId);
@@ -168,7 +167,7 @@ const clients = new Map();
   socket.on("error", (error) => {
     console.error(`Error en Socket.IO para usuario ${socket.userId}:`, error);
   });
-});*/
+});
 
 module.exports = { server, io };
 
