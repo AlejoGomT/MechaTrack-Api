70c1ccaac02b412ccbf88b86db9e37f1ff6c42f4
Alejandro Gomez  T. <alejogomezt2000@gmail.com>
2025-06-04 20:23:35 -0300

    Cambio de configuración de la base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -2,14 +2,14 @@ const { Pool } = require("pg");
 require("dotenv").config();
 const config = require("./config");
 
-const pool = new Pool(config.db);
+//const pool = new Pool(config.db);
 
-/*const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
       : false,
-});*/
+});
 
 module.exports = pool;
